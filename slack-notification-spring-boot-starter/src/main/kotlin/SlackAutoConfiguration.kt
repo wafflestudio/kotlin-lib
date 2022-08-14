915d62df4dc6bef3c3a8a768e8d7dad8c4e25d83
jon <lgock2023@gmail.com>
2022-08-14 23:15:34 +0900

    define core class (WIP)

---
+++ slack-notification-spring-boot-starter/src/main/kotlin/SlackAutoConfiguration.kt
@@ -0,0 +1,51 @@
+package io.wafflestudio.spring.slack
+
+import io.wafflestudio.spring.slack.webflux.SlackWebExceptionHandler
+import io.wafflestudio.spring.slack.webmvc.SlackExceptionResolver
+import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SlackAutoConfiguration {
+
+    @Bean
+    fun slackClient(): SlackClient {
+        // TODO
+        return object : SlackClient {
+            private val logger = LoggerFactory.getLogger(javaClass)
+
+            override fun isEnabled() {
+                logger.debug("isEnabled")
+            }
+
+            override fun captureEvent(e: SlackEvent) {
+                logger.debug("captureEvent e: {}", e)
+            }
+        }
+    }
+
+    @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+    @Configuration
+    class SlackWebMvcConfiguration {
+
+        @Bean
+        fun exceptionResolver(): SlackExceptionResolver {
+            TODO()
+        }
+    }
+
+    @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
+    @Configuration
+    class SlackWebfluxConfiguration(
+        private val slackClient: SlackClient
+    ) {
+
+        @Bean
+        fun exceptionHandler(): SlackWebExceptionHandler {
+            // TODO
+            return SlackWebExceptionHandler(slackClient)
+        }
+    }
+}
